dcba102f968049e4a0cb4b661b2ce270c7ac8f2b
Jannael Orlando <orlandojannael@gmail.com>
2026-05-21 18:15:46 -0600

    feat: deploy in wrangler only for this branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
+import cloudflare from '@astrojs/cloudflare'
 import devsync, { defaultLang, languages } from './src/devsync/devsync'
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
